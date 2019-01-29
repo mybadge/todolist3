388030c7a595dd4a344497ede189b79d90ab7adf
mybadge <mybadge@163.com>
2019-01-29 18:06:33 +0800

    修改样式引入的位置.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Button from 'antd/lib/button';
 
-import './App.css';
+
 
 class App extends Component {
   render() {
